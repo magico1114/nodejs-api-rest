605a17adca400751c7382c5c5236155807fb4b48
Agustín Camero <magico1114@gmail.com>
2019-12-03 02:51:33 -0300

    Check .env API_KEY(token) for dev

--- middlewares/validToken.js
+++ middlewares/validToken.js
@@ -1,23 +1,28 @@
 const jwt = require('jsonwebtoken');
 const { JWT } = require('../config');
+const env = require("../env");
 
 // Really basic token verification!
 // TODO: More complete token verification with decode method of jwt to check if the token has been expired or log data.
 const validToken = (req, res, next) => {
-    const token = req.headers['access-token'];
+  let token = undefined;
+  if ( env.isDevelopment && env.API_KEY )
+    token = env.API_KEY;
+  else
+    token = req.headers['access-token'];
 
-    if (!token)
-      return res.status(401).send({ error: "Missing token" });
+  if (!token)
+    return res.status(401).send({ error: "Missing token" });
 
-    // Syncronic jwt verification in a few lines
-    try {
-      let data = jwt.verify(token, JWT.key, JWT.options);
-      console.log(JSON.stringify(data));
-      next();
-    } catch (e) {
-      //console.log("Error: " + e);
-      return res.status(401).send({ error: "Invalid token" });
-    }
- };
+  // Syncronic jwt verification in a few lines
+  try {
+    let data = jwt.verify(token, JWT.key, JWT.options);
+    console.log(JSON.stringify(data));
+    next();
+  } catch (e) {
+    //console.log("Error: " + e);
+    return res.status(401).send({ error: "Invalid token" });
+  }
+};
 
- module.exports = validToken;
+module.exports = validToken;
