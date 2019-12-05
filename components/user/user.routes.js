bcd9a367a827244fdde36a1732bbb74331bdab13
Agustín Camero <magico1114@gmail.com>
2019-12-04 22:56:29 -0300

    Fix for test

--- components/user/user.routes.js
+++ components/user/user.routes.js
@@ -4,6 +4,6 @@ const userController = require('./user.controller');
 routes.post('/', userController.add);
 routes.get('/all', userController.getAll); // for dev porpouse
 routes.get('/:id', userController.get);
-
+routes.delete('/:id', userController.remove);
 
 module.exports = routes;
